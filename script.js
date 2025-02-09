8dc8ed87a2629dbeb44beacba5ef5ab3564beb57
Idunnomyname <jsjsjsjsj@gmail.com>
2025-02-09 22:54:52 +0530

    i dont know what i m doing

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ const messages = [
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Just kidding, say yes please!"
 ];
 
 let messageIndex = 0;
